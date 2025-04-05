2ecdcfa7ae090e09daa7db0887153f7326fee5b6
Mukumba <119177054+Mukumbaa@users.noreply.github.com>
2025-04-05 19:36:40 +0200

    better c2asm

--- c2asm/main.c
+++ c2asm/main.c
@@ -3,8 +3,22 @@
 #include <process.h>
 
 int main(int argc, char **argv){
-  char *fileName = argv[1];
-  // printf("%s\n",fileName);
-  execlp("gcc", "gcc", "-S", "-fno-asynchronous-unwind-tables", fileName, "-o", "output.asm", "-masm=intel", (char *)NULL);  
+
+  // printf("%d",argc);
+  char *fileName = NULL;
+  char *outputFileName = NULL;
+  
+  if(argc > 1 && argc <= 3){
+    fileName = argv[1];
+  }else{
+    printf("Too few or too many arg");
+    return 0;
+  }
+  if(argc == 3){
+    outputFileName = argv[2];
+    _execlp("gcc", "gcc", "-S", "-fno-asynchronous-unwind-tables", fileName, "-o", outputFileName, "-masm=intel", (char *)NULL, (char *)NULL);  
+  }else{
+    _execlp("gcc", "gcc", "-S", "-fno-asynchronous-unwind-tables", fileName, "-o", "output.asm", "-masm=intel", (char *)NULL);  
+  }
   return 0;
 }
