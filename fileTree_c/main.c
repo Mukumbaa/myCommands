8234eea603561d6bb871b4844fb998b9492654bc
Mukumba <119177054+Mukumbaa@users.noreply.github.com>
2025-04-10 20:19:27 +0200

    ftree in c

--- fileTree_c/main.c
+++ fileTree_c/main.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <sys/stat.h>   // Per stat()
 #include <string.h>     // Per strcmp()
-
-// Includi la versione portabile di dirent.h per Windows
 #include "libs/dirent.h"
 
 #define MAX_PATH_LEN 1024
 
-// #define SYMB1 '└'└─├│
-// #define SYMB2 '─'
-// #define SYMB3 '├'
-// #define SYMB4 '│'
-
-void list_files_recursively(const char *base_path, int depth) {
+int fileTreeRec(const char *base_path, int depth) {
     DIR *dir;
     struct dirent *entry;
     struct stat statbuf;
     char path[MAX_PATH_LEN];
+    int numOfFiles = 0;
 
     if ((dir = opendir(base_path)) == NULL) {
         perror("opendir");
-        return;
+        return -1;
     }
-
+    
     while ((entry = readdir(dir)) != NULL) {
-        // Salta "." e ".."
+        // skip "." end ".."
         // if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-        if (entry->d_name[0] == '.')
+        if (entry->d_name[0] == '.'){
             continue;
+        }
 
         // Costruisce il percorso completo (usa / o \ a seconda del sistema)
         snprintf(path, sizeof(path), "%s/%s", base_path, entry->d_name);
@@ -36,27 +31,34 @@ void list_files_recursively(const char *base_path, int depth) {
             perror("stat");
             continue;
         }
-
-        // Stampa indentazione
+        // indentation
         for (int i = 0; i < depth; i++) {
-            printf("    ");
+            if(i < 1){
+                printf("  ");
+            }
+            else{
+                printf("|  ");
+            }
+        }
+        if(!(depth < 1)){
+            printf("--");
         }
 
         if (S_ISDIR(statbuf.st_mode)) {
             // È una directory
             printf("%s/\n", entry->d_name);
-            list_files_recursively(path, depth + 1);  // Ricorsione
+            int isEmpty = fileTreeRec(path, depth + 1) > 0 ? 0 : 1;
         } else {
             // È un file
             printf("%s\n", entry->d_name);
         }
+        numOfFiles++;
     }
-
     closedir(dir);
+    return numOfFiles;
 }
 
 int main() {
-        printf("%c\n", (unsigned char)195);
-    list_files_recursively(".", 0);  // Parte dalla directory corrente
+    fileTreeRec(".", 0);
     return 0;
 }
