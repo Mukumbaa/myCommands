751df9dfa4451fd82dd3de1e484fb6ef51070ec6
Mukumba <119177054+Mukumbaa@users.noreply.github.com>
2025-05-14 09:25:43 +0200

    fixed rgx
    1) multi match for one string
    2) fixed case oneOrMore

--- rgx/src/main.c
+++ rgx/src/main.c
@@ -100,32 +100,70 @@ bool match_regex(regex_e *regex, int size, String input, int *s_idx) {
       }
       (*s_idx)++;
       while ((*s_idx) < input.len && match_here(input.str[(*s_idx)], curr)) {
-        s_idx++;
+        (*s_idx)++;
       }
     }
   }
   // return s_idx == input.len;
   return true;
 }
+// bool match_regex_anywhere(regex_e *regex, int size, String input) {
+//   int s_idx = 0;
+//   int count = 0;
+//   for (int start = 0; start < input.len; start++) {
+//     // printf("start%d\n",start);
+//     String sliced = {.str = &input.str[start], .len = input.len - start};
+//     // printf("!%s-%d\n",sliced.str,sliced.len);
+//     if (match_regex(regex, size, sliced, &s_idx)) {
+//       count++;
+//       printf("start:%d-s_idx:%d\n",start,s_idx);
+//       printf("Match: %s\n", input.str);
+//       printf("       ");
+//       for (int i = 0; i < start; i++) {
+//         printf(" ");
+//       }
+//       for (int i = 0; i < s_idx; i++) {
+//         printf("~");
+//       }
+//       printf("\n");
+//       // return true;
+//     }
+//   }
+//   if (count != 0){
+//     return true;
+//   }
+//   printf("No match\n");
+//   return false;
+// }
 bool match_regex_anywhere(regex_e *regex, int size, String input) {
-  int s_idx = 0;
-  for (int start = 0; start < input.len; start++) {
-    // printf("start%d\n",start);
+  int start = 0;
+  int count = 0;
+
+  while (start < input.len) {
+    int s_idx = 0;
     String sliced = {.str = &input.str[start], .len = input.len - start};
-    // printf("!%s-%d\n",sliced.str,sliced.len);
-    if (match_regex(regex, size, sliced, &s_idx)) {
+
+    if (match_regex(regex, size, sliced, &s_idx) && s_idx > 0) {
+      count++;
+      printf("start:%d-s_idx:%d\n", start, s_idx);
       printf("Match: %s\n", input.str);
       printf("       ");
-      for (int i = 0; i < start; i++) {
+      for (int i = 0; i < start; i++)
         printf(" ");
-      }
-      for (int i = 0; i < s_idx; i++) {
+      for (int i = 0; i < s_idx; i++)
         printf("~");
-      }
       printf("\n");
-      return true;
+
+      start += s_idx; // Avanza fino alla fine del match
+    } else {
+      start++; // Nessun match qui, avanza di uno
     }
   }
+
+  if (count != 0) {
+    return true;
+  }
+
   printf("No match\n");
   return false;
 }
